a43381fbc79c3680d3b4f21634c376259e0e8a90
Safe <eemarabs@gmail.com>
2023-04-03 12:46:47 +0100

    added new images and dark mode

---
+++ script.js
@@ -0,0 +1,119 @@
+//Hamburger Menu
+const hamburgerBtn = document.querySelector('.hamburgerIcon')
+const mobileNav = document.querySelector('.mobileNav')
+hamburgerBtn.addEventListener('click', function(){
+    if(mobileNav.classList.contains('hide')){
+        console.log('hello')
+        mobileNav.classList.remove('hide')
+        mobileNav.classList.add('show')
+    }else{
+        mobileNav.classList.add('hide')
+        mobileNav.classList.remove('show')
+    }
+})
+//Acccordion
+const accordionHeaders = document.querySelectorAll('.accordionHeader');
+
+accordionHeaders.forEach(header => {
+  header.addEventListener('click', () => {
+    header.classList.toggle('active');
+    const arrowIcon = document.querySelectorAll('.accordionHeader img')
+
+    const accordionContent = header.nextElementSibling;
+    if (accordionContent.style.display === 'block') {
+      accordionContent.style.display = 'none';
+      arrowIcon.innerHTML = '<img src="Images/dark-mode-dash-icon.png width="24px" height="24px" alt="">';
+
+      console.log('work')
+    } else {
+      accordionContent.style.display = 'block';
+      console.log('working')
+    }
+  });
+});
+
+const modeIcon = document.querySelector('.modeIcon');
+const searchIcon = document.querySelector('.searchIcon')
+const hamburgerIcon = document.querySelector('.hamburgerIcon')
+
+modeIcon.addEventListener('click', function(){
+  if (document.body.classList.contains('darkMode')) {
+    document.body.classList.remove('darkMode');
+    document.body.classList.add('lightMode');
+    modeIcon.innerHTML = '<img src="Images/light-mode-icon.png" width="24px" height="24px" alt="">';
+    searchIcon.innerHTML = '<img src="Images/light-mode-search-icon.png" width="24px" height="24px" alt="">';
+    hamburgerIcon.innerHTML = '<img src="Images/hamburger-light-mode-icon.png" width="24px" height="24px" alt="">';
+
+  } else {
+    document.body.classList.remove('lightMode');
+    document.body.classList.add('darkMode');
+    modeIcon.innerHTML = '<img src="Images/dark-mode-icon.png" width="24px" height="24px" alt="">';
+    searchIcon.innerHTML = '<img src="Images/dark-mode-search-icon.png" width="24px" height="24px" alt="">';
+    hamburgerIcon.innerHTML = '<img src="Images/hamburger-dark-mode-icon.png" width="24px" height="24px" alt="">';
+
+
+  }
+});
+const carouselSlide = document.querySelector('.carouselSlide');
+const carouselImages = document.querySelectorAll('.carouselSlide img');
+const dots = document.querySelectorAll('.dot');
+const prevButton = document.querySelector('.prev-slide');
+const nextButton = document.querySelector('.next-slide');
+
+let counter = 1;
+const slideWidth = carouselImages[0].clientWidth;
+
+carouselSlide.style.transform = `translateX(${-slideWidth * counter}px)`;
+
+dots.forEach((dot, index) => {
+  dot.addEventListener('click', () => {
+    carouselSlide.style.transition = 'transform 0.4s ease-in-out';
+    counter = index + 1;
+    carouselSlide.style.transform = `translateX(${-slideWidth * counter}px)`;
+    updateDots();
+  });
+});
+
+nextButton.addEventListener('click', () => {
+  if (counter >= carouselImages.length - 1) return;
+  carouselSlide.style.transition = 'transform 0.4s ease-in-out';
+  counter++;
+  carouselSlide.style.transform = `translateX(${-slideWidth * counter}px)`;
+  updateDots();
+});
+
+prevButton.addEventListener('click', () => {
+  if (counter <= 0) return;
+  carouselSlide.style.transition = 'transform 0.4s ease-in-out';
+  counter--;
+  carouselSlide.style.transform = `translateX(${-slideWidth * counter}px)`;
+  updateDots();
+});
+
+function updateDots() {
+  dots.forEach((dot, index) => {
+    if (index === counter) {
+      dot.classList.add('active');
+    } else {
+      dot.classList.remove('active');
+    }
+  });
+}
+
+function checkImagePosition() {
+  if (carouselImages[counter].id === 'last-clone') {
+    carouselSlide.style.transition = 'none';
+    counter = carouselImages.length - 2;
+    carouselSlide.style.transform = `translateX(${-slideWidth * counter}px)`;
+    updateDots();
+  }
+
+  if (carouselImages[counter].id === 'first-clone') {
+    carouselSlide.style.transition = 'none';
+    counter = carouselImages.length - counter;
+    carouselSlide.style.transform = `translateX(${-slideWidth * counter}px)`;
+    updateDots();
+  }
+}
+
+carouselSlide.addEventListener('transitionend', checkImagePosition);
